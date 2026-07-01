209e21d1e3dc8f2f280e3ea25a294a6a5ff9a0af
Mika Cohen <mjc@kernel.org>
2026-07-01 07:43:21 -0600

    Refine speed app run script

--- scripts/run-speed-app.sh
+++ scripts/run-speed-app.sh
@@ -11,9 +11,11 @@ fi
 
 ./scripts/smoke-swift-package.sh
 
+package_dir="target/swift-package-smoke/CutoutMobile"
+swift_cmd=(env -u SDKROOT -u DEVELOPER_DIR swift)
 export DYLD_LIBRARY_PATH="$root/target/debug:${DYLD_LIBRARY_PATH:-}"
-exec env -u SDKROOT -u DEVELOPER_DIR swift run \
-  --package-path target/swift-package-smoke/CutoutMobile \
+exec "${swift_cmd[@]}" run \
+  --package-path "$package_dir" \
   -Xlinker -L -Xlinker "$root/target/debug" \
   -Xlinker -lcutout_mobile_ffi \
   CutoutMobileSpeedApp
