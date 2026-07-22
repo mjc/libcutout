439293045819a29948ac52d5da510a0d49ef40e1
Mika Cohen <mjc@kernel.org>
2026-07-21 22:55:47 -0600

    refactor: remove unused BMS voltage formatter

--- swift/CutoutMobile/Apps/CutoutApp/BmsVoltageFormatters.swift
+++ swift/CutoutMobile/Apps/CutoutApp/BmsVoltageFormatters.swift
@@ -1,11 +1,6 @@
 import CutoutMobile
 import SwiftUI
 
-private func groupVoltageText(_ snapshot: BmsSnapshot, index: Int?) -> String {
-    guard let index else { return "--" }
-    return groupVoltageText(snapshot.groups.first { $0.index == index })
-}
-
 func groupVoltageText(_ group: BmsGroupSnapshot?) -> String {
     guard let value = group?.voltage?.value else { return "--" }
     return RideUnits.voltageText(millivolts: value, fractionDigits: 3)
