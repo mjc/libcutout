cf292fbf5052d8bd1e2669542adf6a8a9d30afd5
Mika Cohen <mjc@kernel.org>
2026-08-20 03:43:24 -0600

    Separate session driver boundary

--- swift/CutoutMobile/Apps/CutoutApp/CutoutAppModel.swift
+++ swift/CutoutMobile/Apps/CutoutApp/CutoutAppModel.swift
@@ -4,34 +4,6 @@ import Foundation
 import Observation
 
 @MainActor
-protocol CutoutSessionDriving: AnyObject {
-    var onDisplayStateChange: ((RideDisplayState) -> Void)? { get set }
-    var onPhaseChange: ((SessionConnectionPhase) -> Void)? { get set }
-    var onReconnectScheduled: ((SessionConnectionRetry) -> Void)? { get set }
-    var onCaptureEvent: ((CaptureEvent) -> Void)? { get set }
-    var onScanStateChange: ((DevicePickerScanState) -> Void)? { get set }
-    var onSettingsReadbackChange: ((SettingsReadback?) -> Void)? { get set }
-    var onFaultHistoryReadbackChange: ((FaultHistoryReadback?) -> Void)? { get set }
-    var onBmsSnapshotChange: ((BmsSnapshot?) -> Void)? { get set }
-    var onPhoneLocationSnapshotChange: ((MobilePhoneLocationSnapshotDto, MonotonicMilliseconds) -> Void)? { get set }
-    var onProtocolIdentityCandidateChange: ((DevicePickerDiscoveryCandidate?) -> Void)? { get set }
-    var protocolIdentityCandidate: DevicePickerDiscoveryCandidate? { get }
-
-    func start()
-    func pair(platformIdentifier: String) -> Bool
-    func pair(platformIdentifier: String, model: ElectricUnicycleModel) -> Bool
-    func probe(platformIdentifier: String) -> Bool
-    func recordOnly(platformIdentifier: String, note: String?, annotations: [String]) -> Bool
-    func annotateCapture(label: String)
-    func annotateCapture(key: String, value: String)
-    func flushCapture() async -> Bool
-    func disconnectAndScan()
-    func now() -> MonotonicMilliseconds
-}
-
-extension CutoutSessionCore: CutoutSessionDriving {}
-
-@MainActor
 @Observable
 final class CutoutAppModel {
     private(set) var displayState = RideDisplayState()
