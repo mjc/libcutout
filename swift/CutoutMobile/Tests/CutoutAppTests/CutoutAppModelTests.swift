ea051cf2c94cbeee5ce95177786a85e53a4a871a
Mika Cohen <mjc@kernel.org>
2026-09-05 22:25:34 -0600

    Test music capture redaction on history forget

--- swift/CutoutMobile/Tests/CutoutAppTests/CutoutAppModelTests.swift
+++ swift/CutoutMobile/Tests/CutoutAppTests/CutoutAppModelTests.swift
@@ -198,6 +198,7 @@ final class CutoutAppModelTests: XCTestCase {
         XCTAssertTrue(model.musicTimelineEvents.isEmpty)
         XCTAssertEqual(driver.rideMapState.currentMusicHistoryPolicy(), .disabled)
         XCTAssertTrue(driver.rideMapState.currentMusicEvents().isEmpty)
+        XCTAssertNil(driver.musicCaptureObservation)
     }
 
     @MainActor
