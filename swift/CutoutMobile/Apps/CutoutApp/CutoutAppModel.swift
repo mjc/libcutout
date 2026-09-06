220a2c7f02ca323863fdd5806ddaa63e5bfe5e18
Mika Cohen <mjc@kernel.org>
2026-09-05 21:56:40 -0600

    Centralize music provider monitoring changes

--- swift/CutoutMobile/Apps/CutoutApp/CutoutAppModel.swift
+++ swift/CutoutMobile/Apps/CutoutApp/CutoutAppModel.swift
@@ -460,20 +460,25 @@ final class CutoutAppModel {
         musicNowPlaying = projectedMusicNowPlaying()
         musicProviderSelectionStore.set(provider)
         musicTransitionHintTracker.clear()
-        if provider.monitoringMode == .unavailable {
-            musicMonitorSceneState.cancel()
-        }
-#if canImport(MediaPlayer) && os(iOS)
-        if provider.monitoringMode == .unavailable {
-            stopMusicMonitoring()
-        }
-#endif
-        if previousProvider != provider, provider.monitoringMode == .appleMusicSystemPlayer {
-            musicMonitorSceneState.request()
-            beginMusicMonitoring()
-        }
+        updateMusicMonitoring(from: previousProvider, to: provider)
         if !isMusicPlayerHidden {
             connectMusic()
+        }
+    }
+
+    private func updateMusicMonitoring(
+        from previousProvider: MobileMusicProviderDto,
+        to provider: MobileMusicProviderDto
+    ) {
+        switch provider.monitoringMode {
+        case .unavailable:
+            musicMonitorSceneState.cancel()
+            stopMusicMonitoring()
+        case .appleMusicSystemPlayer where previousProvider != provider:
+            musicMonitorSceneState.request()
+            beginMusicMonitoring()
+        case .appleMusicSystemPlayer:
+            break
         }
     }
 
