247dccf5c8e97e98e6a5613b47d1e0878d4966f3
Mika Cohen <mjc@kernel.org>
2026-09-05 20:46:34 -0600

    Redact active music timeline metadata

--- crates/cutout-mobile-ffi/src/lib.rs
+++ crates/cutout-mobile-ffi/src/lib.rs
@@ -6805,10 +6805,14 @@ impl MobileRideMapCore {
             )
             .map_err(map_storage_core_error)?;
         state.music_history_policy = policy;
-        if policy == CoreMusicHistoryPolicy::Disabled {
-            state.music_timeline = cutout_core::MusicTimeline::new();
-        } else if policy == CoreMusicHistoryPolicy::OpaqueItem {
-            state.music_timeline.redact_display_metadata();
+        match policy {
+            CoreMusicHistoryPolicy::Disabled => {
+                state.music_timeline = cutout_core::MusicTimeline::new();
+            }
+            CoreMusicHistoryPolicy::OpaqueItem => {
+                state.music_timeline.redact_display_metadata();
+            }
+            CoreMusicHistoryPolicy::HumanReadable => {}
         }
         Ok(())
     }
