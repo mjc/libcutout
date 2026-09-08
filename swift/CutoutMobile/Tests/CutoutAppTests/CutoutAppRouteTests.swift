ccbf630e3c70e704b8fa38b42cfce504efbd1c73
Mika Cohen <mjc@kernel.org>
2026-09-07 19:58:02 -0600

    Stabilize MELK capability set assertion

--- swift/CutoutMobile/Tests/CutoutAppTests/CutoutAppRouteTests.swift
+++ swift/CutoutMobile/Tests/CutoutAppTests/CutoutAppRouteTests.swift
@@ -1035,7 +1035,7 @@ final class CutoutAppRouteTests: XCTestCase {
         XCTAssertEqual(groups.first?.ids.prefix(3), [1, 2, 212])
         XCTAssertEqual(groups.first(where: { $0.name == "Curtain" })?.ids, Array(57...76))
         XCTAssertTrue((1...212).allSatisfy { LightingPatternCatalog.isMapped($0) })
-        XCTAssertEqual(LightingPatternCatalog.verifiedEffectIDs, [1, 16, 22, 75])
+        XCTAssertEqual(LightingPatternCatalog.verifiedEffectIDs, Set([1, 16, 22, 75]))
         XCTAssertTrue([1, 16, 22, 75].allSatisfy { LightingPatternCatalog.isVerified($0) })
         XCTAssertFalse([0, 2, 212, 213, 227].contains { LightingPatternCatalog.isVerified($0) })
         XCTAssertFalse([0, 213, 220, 255, -1].contains { LightingPatternCatalog.isMapped($0) })
