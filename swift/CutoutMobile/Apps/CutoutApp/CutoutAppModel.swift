8a6b5c3a3dac678bafc1b9aa800e35ea74be813b
Mika Cohen <mjc@kernel.org>
2026-09-08 13:40:41 -0600

    fix: close mobile settings review gaps

--- swift/CutoutMobile/Apps/CutoutApp/CutoutAppModel.swift
+++ swift/CutoutMobile/Apps/CutoutApp/CutoutAppModel.swift
@@ -58,7 +58,7 @@ private extension LightSettingState {
     var lightState: LightState? {
         switch kind {
         case .pending:
-            current
+            requested ?? current
         case .unknown, .current, .confirmed, .refused, .timedOut, .failed:
             current
         }
