41f8a17e986c320805194d048bb17c24487c92ed
Mika Cohen <mjc@kernel.org>
2026-09-13 19:33:36 -0600

    Move rider metrics into Rust projection

--- crates/cutout-core/src/lib.rs
+++ crates/cutout-core/src/lib.rs
@@ -24,6 +24,8 @@ mod session_state;
 pub use session_state::*;
 mod ride_lifecycle;
 pub use ride_lifecycle::*;
+mod rider_dashboard;
+pub use rider_dashboard::*;
 mod energy_estimate;
 pub use energy_estimate::*;
 mod melk_controls;
